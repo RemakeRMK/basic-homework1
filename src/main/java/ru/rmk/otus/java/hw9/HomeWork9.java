7cfcc757342df9e7324cb1b1baadf0e10fcaf785
RemakeRMK <h3yenotqq@gmail.com>
2023-12-21 21:59:23 +0300

    HomeWork 9 final

--- src/main/java/ru/rmk/otus/java/hw9/HomeWork9.java
+++ src/main/java/ru/rmk/otus/java/hw9/HomeWork9.java
@@ -2,32 +2,31 @@ package ru.rmk.otus.java.hw9;
 
 public class HomeWork9 {
     public static void main(String args[]) {
-        try{
+        try {
             int answer = massiveArray(new String[][]{{"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}, {"1", "2", "3", "4"}});
             System.out.println(answer);
-        }catch(ArrayIndexOutOfBoundsException e) {
-            System.out.println("AppArraySizeException");
-        }catch (NumberFormatException e){
-            System.out.println("AppArrayDataException");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (AppArraySizeException e) {
+        } catch (AppArrayDataException e) {
         }
     }
-    public static int massiveArray(String [][] a ) throws Exception {
+
+    public static int massiveArray(String[][] a) throws AppArraySizeException, AppArrayDataException {
         int sum = 0;
-        int num;
-        int[][] massive = new int[4][4];
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                num = Integer.parseInt(a[i][j]);
-                massive[i][j] = num;
-            }
-        }
-        for (int i = 0; i < massive.length; i++) {
-            for (int j = 0; j < massive.length; j++) {
-                sum += massive[i][j];
+                sum += Integer.parseInt(a[i][j]);
             }
         }
         return sum;
     }
+
+    public class AppArraySizeException extends Exception {
+        private static final long serialVersionUID = -5116101128118950844L;
     }
+
+    public
+    class AppArrayDataException extends Exception {
+        static final long serialVersionUID = -2848938806368998894L;
+    }
+
+}
